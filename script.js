6e22f295d73d6965a007000d43861206ef82c307
Stefania <126720391+Aoblu87@users.noreply.github.com>
2023-09-19 20:59:01 +0200

    completata parte della fetch. manca stilare la presentazione delle card

--- script.js
+++ script.js
@@ -22,30 +22,28 @@ function readInputValue() {
 
 // ---------funzione che mostra i risultati della chiamata----------
 function displayImages(result) {
-
+    searchTitle.innerText = ''
+    searchTitle.innerText = inputSearch.value.toUpperCase()
+    cardRow.innerText = ''
     const images = result.photos
 
-    const image = images.map()
-    // for (let i = 0; i < images.length; i++) {
+    const image = images.map(result => {
+        return /*html*/`<div class="col d-flex">
 
-    //     searchTitle.innerText = inputSearch.value.toUpperCase()
-
-    //     cardRow.innerHTML += `<div class="col">
-
-    //     <div class="card">
-    //       <img src="${images[i].src.large}" class="card-img" alt="${images[i].alt}">
-    //       <div class="card-img-overlay d-none">
-    //         <h5 class="card-title">Card title</h5>
-    //         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-    //           content. This content is a little bit longer.</p>
-    //         <p class="card-text"><small>Last updated 3 mins ago</small></p>
-    //       </div>
-    //     </div>
-    //   </div>`
+             <div class="card">
+               <img  src="${result.src.large}" class="card-img" alt="${result.alt}">
+               <div class="card-img-overlay d-none">
+                 <h5 class="card-title"></h5>
+                 <p class="card-text"></p>
+                 <p class="card-text"><small></small></p>
+               </div>
+             </div>
+           </div>`
 
 
-
-    // }
+    })
+    cardRow.innerHTML += image.join('')
+   
 }
 
 
@@ -67,29 +65,12 @@ inputSearch.addEventListener("keypress", function (event) {
     }
 })
 
-//------------------- Rimuovo elementi DOM della precedente ricerca al click del bottone GO
-buttonSearch.addEventListener("click", removeElement)
-inputSearch.addEventListener('keypress', removeElement)
-
-function removeElement() {
-
-
-    // rimuovo titolo sezione
-    searchTitle.innerText = ''
-
-    // rimuovo precedenti risultati
-    while (cardRow.hasChildNodes()) {
-        cardRow.removeChild(cardRow.firstChild)
-    }
-
-}
-
 
 
 // ------------funzione di chiamata API che prende il paramentro dall'input della ricerca
-function getRandomImages(getUrl) {
+function getImages(url) {
 
-    fetch(getUrl(), {
+    fetch(url, {
         headers: {
             "Authorization": "U5PLOUXncn0NuIT9U377ZYNAd90KP3DUmGHi3M4MPXQ2dkE1Yog1i8Eq"
         }
@@ -98,3 +79,5 @@ function getRandomImages(getUrl) {
         .then(displayImages)
         .catch(error => console.log('error', error));
 }
+
+getImages("https://api.pexels.com/v1/search?query=japan")
